9e1e438a865d8caf2c8a3a6fb21373be829e9edb
Eray Ates <eray.ates@priviasecurity.com>
2024-10-18 17:57:33 +0300

    init: Project initialized.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        bgGradient: "linear-gradient(180deg, #F5F7FA 0%, #FFFFFF 100%)",
+        buttonGradient:
+          "linear-gradient(239.78deg, #148DFB 4.19%, #002F88 91.75%)",
+        textGradient:
+          "linear-gradient(62.82deg, #07DEFD 0.95%, #1476FB 93.47%)",
+      },
     },
   },
   plugins: [],
