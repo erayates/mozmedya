a7444d41efedf754365a8c8787b1b5e0a5bf54ad
Eray Ates <eray.ates@priviasecurity.com>
2024-10-23 14:43:27 +0300

    fix: -

--- src/containers/news-software-container/admin-panel/index.tsx
+++ src/containers/news-software-container/admin-panel/index.tsx
@@ -4,18 +4,23 @@ import Image from "next/image";
 
 const NewsSoftwareAdminPanel: React.FC = () => {
   return (
-    <section id="yönetici-paneli" className="container-auto mt-32">
+    <section id="yönetici-paneli" className="container-auto mt-32 ">
       <SectionHeader
         title="Tamamen işinize odaklanın, gerisini bize bırakın!"
         subtitle="HABER YAZILIMI DENEYİMİNİZİ YENİDEN TANIMLIYORUZ!"
         description="Yenilenen alt yapı sayesinde, işinizdeki potansiyeli tam anlamıyla keşfedin ve güncel kalmanın yanı sıra güvenli bir şekilde yol alın. Artık sadece işinize odaklanın, çünkü geliştirilmiş alt yapı sayesinde tüm detayları biz yönetiyoruz."
+        className="max-w-[974px]"
       />
 
-      <div className="grid grid-cols-[1.25fr_1.75fr] gap-8 mt-20">
+      <div className="grid grid-cols-[1.25fr_1.75fr] gap-8 mt-20 ml-8">
         <div className="flex flex-col py-3">
           <div className="group mb-8 border-b-[3px] relative pb-6 border-dark-cream overflow-hidden before:w-0 before:h-[3px] before:bg-ghost before:absolute before:bottom-0 before:left-1 hover:before:w-full after:absolute after:left-0 after:bottom-0 after:z-20 after:bg-textGradient after:w-0 after:h-[3px] hover:after:w-full hover:after:transition-all hover:after:duration-[3000ms] hover:border-none">
             <h3 className="space-x-2 flex items-center">
-              <Elements width={22} height={22} className="grayscale group-hover:grayscale-0 group-hover-animate" />
+              <Elements
+                width={22}
+                height={22}
+                className="grayscale group-hover:grayscale-0 group-hover-animate"
+              />
               <span className="text-secondary group-hover:text-primary font-semibold text-lg leading-6">
                 Gelişmiş Eklenti Sistemi
               </span>
@@ -28,7 +33,11 @@ const NewsSoftwareAdminPanel: React.FC = () => {
 
           <div className="group mb-8 border-b-[3px] relative pb-6 border-dark-cream overflow-hidden before:w-0 before:h-[3px] before:bg-ghost before:absolute before:bottom-0 before:left-1 hover:before:w-full after:absolute after:left-0 after:bottom-0 after:z-20 after:bg-textGradient after:w-0 after:h-[3px] hover:after:w-full hover:after:transition-all hover:after:duration-[3000ms] hover:border-none">
             <h3 className="space-x-2 flex items-center">
-              <Elements width={22} height={22} className="grayscale group-hover:grayscale-0 group-hover-animate" />
+              <Elements
+                width={22}
+                height={22}
+                className="grayscale group-hover:grayscale-0 group-hover-animate"
+              />
               <span className="text-secondary group-hover:text-primary font-semibold text-lg leading-6">
                 Gelişmiş Eklenti Sistemi
               </span>
@@ -41,7 +50,11 @@ const NewsSoftwareAdminPanel: React.FC = () => {
 
           <div className="group mb-8 border-b-[3px] relative pb-6 border-dark-cream overflow-hidden before:w-0 before:h-[3px] before:bg-ghost before:absolute before:bottom-0 before:left-1 hover:before:w-full after:absolute after:left-0 after:bottom-0 after:z-20 after:bg-textGradient after:w-0 after:h-[3px] hover:after:w-full hover:after:transition-all hover:after:duration-[3000ms] hover:border-none">
             <h3 className="space-x-2 flex items-center">
-              <Elements width={22} height={22} className="grayscale group-hover:grayscale-0 group-hover-animate" />
+              <Elements
+                width={22}
+                height={22}
+                className="grayscale group-hover:grayscale-0 group-hover-animate"
+              />
               <span className="text-secondary group-hover:text-primary font-semibold text-lg leading-6">
                 Gelişmiş Eklenti Sistemi
               </span>
@@ -52,10 +65,13 @@ const NewsSoftwareAdminPanel: React.FC = () => {
             </p>
           </div>
 
-
           <div className="group mb-8 border-b-[3px] relative pb-6 border-dark-cream overflow-hidden before:w-0 before:h-[3px] before:bg-ghost before:absolute before:bottom-0 before:left-1 hover:before:w-full after:absolute after:left-0 after:bottom-0 after:z-20 after:bg-textGradient after:w-0 after:h-[3px] hover:after:w-full hover:after:transition-all hover:after:duration-[3000ms] hover:border-none">
             <h3 className="space-x-2 flex items-center">
-              <Elements width={22} height={22} className="grayscale group-hover:grayscale-0 group-hover-animate" />
+              <Elements
+                width={22}
+                height={22}
+                className="grayscale group-hover:grayscale-0 group-hover-animate"
+              />
               <span className="text-secondary group-hover:text-primary font-semibold text-lg leading-6">
                 Gelişmiş Eklenti Sistemi
               </span>
@@ -66,10 +82,13 @@ const NewsSoftwareAdminPanel: React.FC = () => {
             </p>
           </div>
 
-          
           <div className="group mb-8 border-b-[3px] relative pb-6 border-dark-cream overflow-hidden before:w-0 before:h-[3px] before:bg-ghost before:absolute before:bottom-0 before:left-1 hover:before:w-full after:absolute after:left-0 after:bottom-0 after:z-20 after:bg-textGradient after:w-0 after:h-[3px] hover:after:w-full hover:after:transition-all hover:after:duration-[3000ms] hover:border-none">
             <h3 className="space-x-2 flex items-center">
-              <Elements width={22} height={22} className="grayscale group-hover:grayscale-0 group-hover-animate" />
+              <Elements
+                width={22}
+                height={22}
+                className="grayscale group-hover:grayscale-0 group-hover-animate"
+              />
               <span className="text-secondary group-hover:text-primary font-semibold text-lg leading-6">
                 Gelişmiş Eklenti Sistemi
               </span>
@@ -81,8 +100,8 @@ const NewsSoftwareAdminPanel: React.FC = () => {
           </div>
         </div>
 
-        <div className="bg-textGradient rounded-2xl relative overflow-hidden">
-          <div className="max-w-[715px] max-h-[437px] absolute -right-24 bottom-0">
+        <div className="bg-textGradient max-w-[665px] rounded-2xl relative overflow-hidden shadow-reference-card">
+          <div className="max-w-[745px] max-h-[437px] absolute -right-[140px] -bottom-8">
             <Image
               src="/assets/images/hero/browser.png"
               alt="Mozmedya Tarayıcı"
